5d07428c872432f53d609b6e53952d8afaf15968
Théo Fénique <theo.fenique@gmail.com>
2018-09-27 21:02:38 +0200

    Finished home and inspirations

---
+++ js/inspirationsAnim.js
@@ -0,0 +1,49 @@
+let leftButton = document.querySelector('.waterLink')
+let rightButton = document.querySelector('.landLink')
+let leftTxt = document.querySelector('.txtWater')
+let rightTxt = document.querySelector('.txtLand')
+let background = document.querySelector('.inspiBg')
+
+leftButton.addEventListener(
+  'click',
+  function(e){
+    e.preventDefault()
+    if (leftTxt.classList.contains('activeLeft')==true){
+      console.log('pute')
+      background.style.left ='-50%'
+      leftButton.style.left = '50%'
+      rightButton.style.left = '50%'
+      leftTxt.classList.remove('activeLeft')
+    }
+    else{
+      background.style.left = '0%'
+      leftButton.style.left = '100%'
+      rightButton.style.left = '100%'
+      leftTxt.classList.add('activeLeft')
+    }
+
+  },
+  false
+)
+
+rightButton.addEventListener(
+  'click',
+  function(e){
+    e.preventDefault()
+    if (rightTxt.classList.contains('activeRight')==true){
+      console.log('pute')
+      background.style.left ='-50%'
+      leftButton.style.left = '50%'
+      rightButton.style.left = '50%'
+      rightTxt.classList.remove('activeRight')
+    }
+    else{
+      background.style.left = '-100%'
+      leftButton.style.left = '0%'
+      rightButton.style.left = '0%'
+      rightTxt.classList.add('activeRight')
+    }
+
+  },
+  false
+)
